eb2cb6f352aa2a9af627631c3bc9ff363f69a97c
samuelwondimu <wsamuelwondimu@gmail.com>
2021-10-07 16:51:26 +0300

    all

---
+++ Desktop/e-commerce/e_commerce/src/App.tsx
@@ -0,0 +1,15 @@
+import { CssBaseline, Typography } from "@mui/material";
+import React from "react";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Typography>products</Typography>
+    </>
+  );
+}
+
+export default App;
